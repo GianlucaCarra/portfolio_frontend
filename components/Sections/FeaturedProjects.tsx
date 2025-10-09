d955ce96cde4aa3403c4c611a0d3af0243406dab
Gianluca Carra <contact@giancarra.com>
2025-10-09 21:30:39 +0200

    fix: added return on prerender

--- components/Sections/FeaturedProjects.tsx
+++ components/Sections/FeaturedProjects.tsx
@@ -5,6 +5,8 @@ import { ProjectCarousel } from "../UI/ProjectCarousel/ProjectCarousel";
 export async function FeaturedProjects() {
 	const { data } = await getProjects();
 
+	if (!data || data.length === 0) return null;
+
 	return (
 		<section className="flex flex-col gap-3" id="featured">
 			<SectionTitle text="Featured Projects" />
